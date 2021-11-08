15f96e37d0f8afb228de11b3d26d64168b7c2a28
luigi059 <luigimarkfernandez@gmail.com>
2021-11-08 16:01:16 +0000

    working on posting user data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,27 @@
 const sql = require('mssql');
 
-exports.getUsers = async (req,res) => {
+exports.getUsers = async (req, res) => {
   try {
-    const users = await sql.query("select * from UsersTBL");
-    res.send(users);
+    const users = await sql.query('select * from UsersTBL');
+    res.send(users.recorset);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
   }
-  catch(err){
-    res.send(err);
+};
+
+exports.addUser = async (req, res) => {
+  try {
+    const { userID, username, email, password } = req.body;
+    const request = new sql.Request();
+    await request
+      .request()
+      .input('user_id', userID)
+      .input('user_name', username)
+      .input('user_email', email)
+      .input('user_password', password)
+      .execute('addUser');
+    res.send('Created a new User');
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
   }
-}
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
 
-router.route('/').get(userController.getUsers);
+router.route('/').get(userController.getUsers).post(userController.addUser);
 module.exports = router;
