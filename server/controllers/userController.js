d1de39997b991dcc6fc75203e4dd97c40764f9c0
luigi059 <luigimarkfernandez@gmail.com>
2021-11-19 20:34:37 +0000

    refactoring, password hashing, added validation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,32 +1,44 @@
-const sql = require('mssql');
 const sqlcon = require('../dbconnection');
+const bcrypt = require('bcrypt');
 
-exports.getUsers = async (req, res) => {
-  console.log('welcome to getUsers controller');
-  try {
-    const conn = await sqlcon.getConnection();
-    const users = await conn.query('select * from UsersTBL');
-    res.send(users.recordset);
-  } catch (err) {
-    res.status(500).json({ msg: err.message });
+class userController {
+  // For Testing Only
+  async getUsers(req, res) {
+    console.log('welcome to getUsers controller');
+    try {
+      const conn = await sqlcon.getConnection();
+      const users = await conn.query('select * from UsersTBL');
+      res.send(users.recordset);
+    } catch (err) {
+      res.status(500).json({ msg: err.message });
+    }
   }
-};
 
-exports.addUser = async (req, res) => {
-  console.log('welcom to addUser controller');
-  try {
-    const conn = await sqlcon.getConnection();
-    const { userID, username, email, password } = req.body;
-    console.log(username);
-    await conn
-      .request()
-      .input('user_id', userID)
-      .input('user_name', username)
-      .input('user_email', email)
-      .input('user_password', password)
-      .execute('addUser');
-    res.send('Created a new User');
-  } catch (err) {
-    res.status(500).json({ msg: err.message });
+  async register(req, res) {
+    console.log('welcom to addUser controller');
+    try {
+      const conn = await sqlcon.getConnection();
+      const { username, email, password } = req.body;
+      // Check if there's existing users in the database
+      const existingUser = await conn
+        .request()
+        .input('user_email', email)
+        .execute('getUser');
+      if (existingUser.rowsAffected[0] == 1)
+        return res.status(400).json({ msg: 'This user already exists' });
+      // Hash the Password
+      const passwordHash = await bcrypt.hash(password, 10);
+      // Save to database
+      await conn
+        .request()
+        .input('user_name', username)
+        .input('user_email', email)
+        .input('user_password', { password: passwordHash }.password)
+        .execute('addUser');
+      res.send('Created a new User');
+    } catch (err) {
+      res.status(500).json({ msg: err.message });
+    }
   }
-};
+}
+module.exports = new userController();
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
 
-router.route('/').get(userController.getUsers).post(userController.addUser);
+router.route('/').get(userController.getUsers).post(userController.register);
 module.exports = router;
