6a0ea84e4f418dac4fa8fa16b7a206905e5fda5e
luigi059 <luigimarkfernandez@gmail.com>
2021-12-08 17:07:10 +0000

    full support of request parameters

--- client/src/app/services/card-stack-service.service.ts
+++ client/src/app/services/card-stack-service.service.ts
@@ -1,43 +1,51 @@
 import { Injectable } from '@angular/core';
 import { IcardStack } from '../interfaces/icard-stack';
 
-import { HttpClient, HttpErrorResponse, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest, HttpEvent, HttpBackend } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHandler,
+  HttpHeaders,
+  HttpInterceptor,
+  HttpRequest,
+  HttpEvent,
+  HttpBackend,
+} from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators'
+import { catchError, map, tap } from 'rxjs/operators';
 import { BackendService } from './backend.service';
 import { User } from '../interfaces/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CardStackServiceService {
+  constructor(
+    private http: HttpClient,
+    private backEndService: BackendService
+  ) {}
 
-  constructor(private http: HttpClient, private backEndService : BackendService) { }
+  // Example userID, normally we would get this once the user logs in
+  userID = 25;
+  // Example use of request parameters
+  private deckUri = `http://localhost:3000/user/${this.userID}/decks`;
 
-  
+  getCardStacks(): Observable<IcardStack[]> {
+    console.log('Get card service called');
 
-  private dataUri = 'http://localhost:3000/user/decks'
-
-
-  getCardStacks(): Observable<IcardStack[]>{
-
-    console.log("Get card service called");
-
-    return this.http.get<IcardStack[]>(this.dataUri)
-    .pipe(
-      catchError(this.handleError)
-    )
+    return this.http
+      .get<IcardStack[]>(this.deckUri)
+      .pipe(catchError(this.handleError));
   }
 
   //____________________ addCardToStackMethod post http://localhost:3000/user/decks req.body
 
-  addCardToStack(cardStackDetails : IcardStack): Observable<IcardStack>{
-    console.log("Post cardStack service called");
+  addCardToStack(cardStackDetails: IcardStack): Observable<IcardStack> {
+    console.log('Post cardStack service called');
 
-    return this.http.post<IcardStack>(this.dataUri, JSON.stringify(cardStackDetails))
-    .pipe(
-      catchError(this.handleError)
-    )
+    return this.http
+      .post<IcardStack>(this.deckUri, JSON.stringify(cardStackDetails))
+      .pipe(catchError(this.handleError));
   }
 
   private handleError(error: HttpErrorResponse) {
@@ -49,10 +57,10 @@ export class CardStackServiceService {
       // The response body may contain clues as to what went wrong.
       console.error(
         `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
+          `body was: ${JSON.stringify(error.error)}`
+      );
     }
     // Return an observable with a user-facing error message.
-    return throwError(
-      'Something bad happened; please try again later.');
+    return throwError('Something bad happened; please try again later.');
   }
 }
